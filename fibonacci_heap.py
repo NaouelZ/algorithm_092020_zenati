31ceb50964fce92a4b8a10cc778aca0f0a1736b9
Naouel <zenati.naouel14@gmail.com>
2020-10-01 10:55:23 +0200

    correction of merge()

--- fibonacci_heap.py
+++ fibonacci_heap.py
@@ -45,27 +45,24 @@ class FibonacciHeap():
     et en français : https://fr.wikipedia.org/wiki/Tas_de_Fibonacci#Implémentation_des_opérations
     """
     class Node:
-        def __init__(self, key, value):
-            self.key = key
+        def __init__(self, value):
             self.value = value
             self.parent = self.child = self.left = self.right = None
 
     root_node, min_node = None, None
     total_nodes = 0
 
-    def insert(self, key, value: int) -> None:
-        node_to_insert = self.Node(key, value)
+    def insert(self, value: int) -> None:
+        node_to_insert = self.Node(value)
         node_to_insert.left = node_to_insert.right = node_to_insert
-
         if self.root_node is None:
             self.root_node = node_to_insert
         else:
             node_to_insert.right = self.root_node.right
             node_to_insert.left = self.root_node
             self.root_node.right.left = node_to_insert
-            self.root_node.right = node_to_insert
-        
-        if self.min_node is None or node_to_insert.key < self.min_node.key:
+            self.root_node.right = node_to_insert  
+        if self.min_node is None or node_to_insert.value < self.min_node.value:
             self.min_node = node_to_insert
         self.total_nodes += 1
         return node_to_insert
@@ -91,26 +88,44 @@ class FibonacciHeap():
         """
         Fusionne deux arbres
         """
-        H = FibonacciHeap()
-        H.root_node, H.min_node = self.root_node, self.min_node
+        F = FibonacciHeap()
+        F.root_node, H.min_node = self.root_node, self.min_node
         last = fibonnaci_heap.root_node.left
-        h2.root_node.left = H.root_node.left
-        H.root_node.left.right = fibonnaci_heap.root_node
-        H.root_node.left = last
-        H.root_node.left.right = H.root_node
-        if fibonnaci_heap.min_node.key < H.min_node.key:
+        fibonnaci_heap.root_node.left = F.root_node.left
+        F.root_node.left.right = fibonnaci_heap.root_node
+        F.root_node.left = last
+        F.root_node.left.right = F.root_node
+        if fibonnaci_heap.min_node.value < F.min_node.value:
             H.min_node = fibonnaci_heap.min_node
         H.total_nodes = self.total_nodes + fibonnaci_heap.total_nodes
-        return H
+        return F
 
-
+## Création de mon premier abre
 f = FibonacciHeap()
 
-f.insert(0,2)
-f.insert(1,4)
-f.insert(2,8)
-f.insert(3,5)
+f.insert(4)
+f.insert(8)
+f.insert(5)
 
-print(f)
-
+## Minimun de f
 m = f.find_min()
+print(m)
+
+## Création du second abre
+f2 = FibonacciHeap()
+
+f2.insert(2)
+f2.insert(12)
+f2.insert(18)
+
+## Minimun de f2
+m2 = f2.find_min()
+print(m2)
+
+## fusion des arbres
+merge = f.merge(f2)
+
+## minimun des 2 arbres fusionner
+merge_min = merge.find_min()
+print(merge_min)
+
